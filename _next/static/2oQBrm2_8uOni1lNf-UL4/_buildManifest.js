0791c32f195490173f56a680e2f9af27ed50ad38
aditya-mitra <55396651+aditya-mitra@users.noreply.github.com>
2020-10-30 07:44:40 +0000

    Deploying to gh-pages from  @ 9c911bb03bf8ad9c38c6e95c3d22e744edcf44a4 🚀

--- _next/static/2oQBrm2_8uOni1lNf-UL4/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002F1a48c3c1.ed70020a75f9dbddaa0b.js","static\u002Fchunks\u002Fpages\u002Findex-b4fc17822e63b6b13928.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-5efcade778114a255845.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
