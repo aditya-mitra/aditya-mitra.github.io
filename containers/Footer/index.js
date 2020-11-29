b55807b68fa99356b26b7711fe08ecd36f66a4fb
aditya-mitra <ultrapokeinstinct@gmail.com>
2020-11-30 00:01:03 +0530

    extend highlighter to built-with built

--- containers/Footer/index.js
+++ containers/Footer/index.js
@@ -8,7 +8,6 @@ import {
     highlight as hightlightClass
 } from "@/styles/extras.module.css";
 
-
 export default function Footer() {
     const { colorMode } = useColorMode();
 
@@ -17,10 +16,11 @@ export default function Footer() {
         highlighter.classList.add(hightlightClass);
         highlighter.id = 'highlighter';
         highlighter.style.display = "block";
+        highlighter.style.overflow = "hidden";
         document.body.appendChild(highlighter);
     }, []);
 
-    const highlightLink = event => {
+    const highlightLink = (event, extend=20 )=> {
         const highlighter = document.getElementById('highlighter');
         const linkCoords = event.target.getBoundingClientRect();
 
@@ -31,26 +31,24 @@ export default function Footer() {
             left: linkCoords.left + window.scrollX
         };
 
-        highlighter.style.width = `${coords.width + 20}px`;
-        highlighter.style.height = `${coords.height + 20}px`;
+        highlighter.style.width = `${coords.width + extend}px`;
+        highlighter.style.height = `${coords.height + extend}px`;
         highlighter.style.transform =
-            `translate(${coords.left - 10}px,${coords.top - 10}px)`;
+            `translate(${coords.left - (extend/2)}px,${coords.top - (extend/2)}px)`;
     }
 
-    const finds = constants.map(constant => {
-        return (
-            <Link key={constant.link} isExternal
-                href={constant.link}
-                zIndex={50}
-            >
-                <Icon
-                    onMouseEnter={event => highlightLink(event)}
-                    as={constant.icon} color={constant.colour}
-                    boxSize={8} margin="0 1rem" position="relative" order={4}
-                />
-            </Link>
-        );
-    });
+    const finds = constants.map((constant) => (
+        <Link key={constant.link} isExternal
+            href={constant.link}
+            zIndex={50}
+        >
+            <Icon
+                onMouseEnter={event => highlightLink(event)}
+                as={constant.icon} color={constant.colour}
+                boxSize={8} margin="0 1rem" position="relative" order={4}
+            />
+        </Link>
+    ));
 
     return (
         <>
@@ -68,8 +66,14 @@ export default function Footer() {
                 <div className="built-with">
                     <div className="border" />
                     My Portfolio Site Built With
-                    <Link isExternal href="https://github.com/aditya-mitra/aditya-mitra.github.io/">
-                        <span className="built">NextJS</span>
+                    <Link
+                        isExternal
+                        href="https://github.com/aditya-mitra/aditya-mitra.github.io/"
+                    >
+                        <span
+                            onMouseEnter={event => highlightLink(event, 5)}
+                            className="built"
+                        >NextJS</span>
                     </Link>
                 </div>
             </footer>
