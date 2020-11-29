c951753ad4fdcfb03586cea00942d5d172de8d46
aditya-mitra <ultrapokeinstinct@gmail.com>
2020-11-29 19:50:33 +0530

    added fireanimation in containers/about refactored styles in styles/aboutContainer to extras.module.css

---
+++ containers/About/fireAnimation.js
@@ -0,0 +1,34 @@
+import { useColorMode } from "@chakra-ui/react";
+import styles from "@/styles/fire.js";
+
+export default function () {
+    const { colorMode } = useColorMode();
+    return (
+        <>
+            <style jsx>
+                {styles}
+            </style>
+            <div
+                className={`container ${colorMode === "dark"?"dark":null}`}
+            >
+                <div className="fire-container">
+                    <div className="fire-left">
+                        <div className="main-fire"></div>
+                        <div className="particle-fire"></div>
+                    </div>
+                    <div className="fire-main">
+                        <div className="main-fire"></div>
+                        <div className="particle-fire"></div>
+                    </div>
+                    <div className="fire-right">
+                        <div className="main-fire"></div>
+                        <div className="particle-fire"></div>
+                    </div>
+                    <div className="fire-bottom">
+                        <div className="main-fire"></div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
--- containers/About/index.js
+++ containers/About/index.js
@@ -1,24 +1,25 @@
 import { useColorMode } from "@chakra-ui/react";
 
-import DeviceChange from "./deviceChange";
+import Fire from "./fireAnimation";
 import AboutMe from "./aboutMe";
 import constants from "./constants";
 
-import styles from "@/styles/aboutContainer";
+import {
+    aboutContainer as containerClass,
+    dark
+} from "@/styles/extras.module.css";
 
 export default function About() {
     const { colorMode } = useColorMode();
+    const darkClass = colorMode === "dark" ? dark : null;
+    const classes = containerClass + " " + darkClass;
     return (
-        <>
-            <style jsx>
-                {styles}
-            </style>
-            <div className={`about-container ${colorMode === "dark"?"dark":null}`}>
-                <DeviceChange />
-                <AboutMe title={constants.title} subTitle={constants.subTitle}
-                    description={constants.description} image={constants.image}
-                    status={constants.status} />
-            </div>
-        </>
+        <div className={classes}>
+            <Fire />
+            <AboutMe title={constants.title} subTitle={constants.subTitle}
+                description={constants.description} image={constants.image}
+                status={constants.status}
+            />
+        </div>
     );
 }
