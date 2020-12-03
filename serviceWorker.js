bc58836f9e99b0a153e2df20c2cee7e2cf47a98b
aditya-mitra <55396651+aditya-mitra@users.noreply.github.com>
2020-12-03 18:55:23 +0000

    Deploying to gh-pages from  @ d40ae89077210d8b912901db16e48a8006bd6ad2 🚀

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,14 +1,55 @@
 const CACHE_NAME = "portfolio-cache";
-const urlsToCache = ["/"];
+const urlsToCache =
+    [
+        "/",
+        "_next",
+        "/favicons",
+        "/fonts",
+        "/images",
+    ];
+const cacheAllowlist = ['portfolio-v2-3'];
 
 self.addEventListener("install", event => {
     const preLoaded = caches.open(CACHE_NAME)
-        .then(cache => cache.addAll(urlsToCache))
+        .then(cache => cache.addAll(urlsToCache));
     event.waitUntil(preLoaded);
 });
 
 self.addEventListener("fetch", event => {
     const response = caches.match(event.request)
-        .then(match => match || fetch(event.request));
+        .then(match => match
+            || fetch(event.request).then(response => {
+                if (!response
+                    || response.status !== 200
+                    || response.type !== 'basic') {
+                    return response;
+                }
+                const responseToCache = response.clone();
+
+                caches
+                    .open(CACHE_NAME)
+                    .then(function (cache) {
+                        cache.put(event.request, responseToCache);
+                    });
+
+                return response;
+            })
+        );
+
     event.respondWith(response);
+});
+
+self.addEventListener('activate', event=> {
+
+    const activates = caches.keys().then(cacheNames => 
+        Promise.all(
+            cacheNames.map(cacheName => {
+                if (cacheAllowlist.indexOf(cacheName) === -1) {
+                    return caches.delete(cacheName);
+                }
+            })
+        )
+    );
+
+    event.waitUntil(activates);
 });
